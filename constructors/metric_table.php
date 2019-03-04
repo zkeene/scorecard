66667356b38d6657007a462bd6306c0548916617
Zac Keene <zkeene@gmail.com>
2019-03-04 15:52:50 -0500

    fix to issue #1

--- constructors/metric_table.php
+++ constructors/metric_table.php
@@ -4,23 +4,7 @@
         <?php
             for ($j=1; $j <5; $j++) {
                 if ($j<count($metric_perf)+1) {
-                    echo '<td>'.$metric_perf[$j]['numerator'].'</td>';
-                } else {
-                    echo '<td></td>';
-                }
-            }
-            echo '</tr><tr>';
-            for ($j=1; $j <5; $j++) {
-                if ($j<count($metric_perf)+1) {
-                    echo '<td>'.$metric_perf[$j]['denominator'].'</td>';
-                } else {
-                    echo '<td></td>';
-                }
-            }
-            echo '</tr><tr>';
-            for ($j=1; $j <5; $j++) {
-                if ($j<count($metric_perf)+1) {
-                        echo '<td>'.$metric_perf[$j]['performance'].'%</td>';
+                        echo '<td>'.$metric_perf[$j]['performance'].'% ('.$metric_perf[$j]['numerator'].'/'.$metric_perf[$j]['denominator'].')</td>';
                 } else {
                         echo '<td></td>';
                 }
