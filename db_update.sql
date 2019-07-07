0554e5c26f1286dd7ed38fdebb7c3117491810a6
Zac Keene <zkeene@gmail.com>
2019-07-07 15:42:21 -0400

    db updates for perf period build

--- db_update.sql
+++ db_update.sql
@@ -1,2 +1,4 @@
-ALTER TABLE `specific_metrics`
-	ADD COLUMN `round_precision` INT(2) UNSIGNED NULL AFTER `weight`;
+ALTER TABLE `service_lines`
+	ADD COLUMN `is_period_based` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `service_line`;
+ALTER TABLE `performances`
+	ADD COLUMN `period_performance` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `import_error`;
