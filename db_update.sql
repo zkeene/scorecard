23f8b2721790c44120e6559deda4d7628e839a81
Zac Keene <zkeene@gmail.com>
2019-08-26 09:30:09 -0400

    def updates

--- db_update.sql
+++ db_update.sql
@@ -1,4 +1,3 @@
-ALTER TABLE `service_lines`
-	ADD COLUMN `is_period_based` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `service_line`;
-ALTER TABLE `performances`
-	ADD COLUMN `period_performance` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `import_error`;
+ALTER TABLE kpnquality.metrics ADD numerator_definition TEXT NULL;
+ALTER TABLE kpnquality.metrics ADD denominator_definition TEXT NULL;
+ALTER TABLE kpnquality.metrics ADD exclusion_definition TEXT NULL;
